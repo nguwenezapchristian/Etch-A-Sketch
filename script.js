5c8c10b1a533efe1e0eebdd417852076b49b1499
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-08-09 12:58:41 +0200

    creating a function to create grids

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const div = document.querySelector("#container");
+function createGrids() {
+for (let i = 0; i < 16; i++) {
+    const grids = document.createElement('div');
+    grids.style.cssText = 'color: blue; background-color: red';
+    div.appendChild(grids);
+    for (let a = 0; a < 16; a++) {
+        const grids = document.createElement('div');
+        grids.style.cssText = 'color: yellow; background-color: red';
+        div.appendChild(grids);
+    }
+}
+}
+createGrids();
