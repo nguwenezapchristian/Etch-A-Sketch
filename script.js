76b1caf7356aa2477881318cc7c0aa56e13cd7b9
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-08-11 20:03:47 +0200

    parsed userinput to number and called createGrids

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const grids = document.querySelector("#container");
 const btn = document.querySelector("#btn");
-const userInput = parseInt(document.querySelector("#user-input"));
+const userInput = document.querySelector("#user-input");
 
-btn.addEventListener('click', createGrids(userInput));
+btn.addEventListener('click', () => {
+    let num = parseInt(userInput.value);
+    createGrids(num);
+});
 
 function createGrids(num) {
-for (let i = 0; i < num; i++) {
-    const row = document.createElement('div');
-    row.id = 'div-row';
-    grids.appendChild(row);
-    for (let a = 0; a < num; a++)
-    {
-        const column = document.createElement('div');
-        column.id = 'div-column';
-        row.appendChild(column);
-        column.addEventListener('mouseover', () => {
-            column.style.background = 'black';
-        })
+    grids.innerHTML = '';
+    for (let i = 0; i < num; i++) {
+        const row = document.createElement('div');
+        row.id = 'div-row';
+        grids.appendChild(row);
+        for (let a = 0; a < num; a++)
+        {
+            const column = document.createElement('div');
+            column.id = 'div-column';
+            row.appendChild(column);
+            column.addEventListener('mouseover', () => {
+                column.style.background = 'black';
+            })
+        }
     }
 }
-}
+
