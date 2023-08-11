c9d0265bc401aa8c68534fe6d25be3867b85d97a
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-08-11 20:40:43 +0200

    added a function to generate random colors

--- script.js
+++ script.js
@@ -4,24 +4,37 @@ const userInput = document.querySelector("#user-input");
 
 btn.addEventListener('click', () => {
     let num = parseInt(userInput.value);
+    grids.innerHTML = '';
     createGrids(num);
 });
 
 function createGrids(num) {
-    grids.innerHTML = '';
+
     for (let i = 0; i < num; i++) {
         const row = document.createElement('div');
-        row.id = 'div-row';
+        row.className = 'div-row';
         grids.appendChild(row);
+
         for (let a = 0; a < num; a++)
         {
             const column = document.createElement('div');
-            column.id = 'div-column';
+            column.className = 'div-column';
             row.appendChild(column);
             column.addEventListener('mouseover', () => {
-                column.style.background = 'black';
+                column.style.background = randomColor();
             })
         }
     }
+}
+function randomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * 6);
+        const randomLetter = letters[randomIndex];
+        color += randomLetter; 
+    }
+    return color;
 }
 
