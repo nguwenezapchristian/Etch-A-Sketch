11da76c104685565cc1811846960fe939fa506f6
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-08-11 16:38:49 +0200

    made column and rows to form a grid

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const div = document.querySelector("#container");
 function createGrids() {
 for (let i = 0; i < 16; i++) {
     const grids = document.createElement('div');
+    grids.id = 'div-row';
     grids.style.cssText = 'color: blue; background-color: red';
     div.appendChild(grids);
-    for (let a = 0; a < 16; a++) {
-        const grids = document.createElement('div');
-        grids.style.cssText = 'color: yellow; background-color: red';
-        div.appendChild(grids);
+    for (let a = 0; a < 16; a++)
+    {
+        const column = document.createElement('div');
+        column.id = 'div-column';
+        column.textContent = 'B';
+        column.style.cssText = 'color: pink; background-color: yellow';
+        grids.appendChild(column);
     }
 }
 }
